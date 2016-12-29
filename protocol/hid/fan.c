44650faa1e638e72cf6aedb2baeec5c945fdf500
Sean Nelson <audiohacked@gmail.com>
2016-12-28 17:25:06 -0800

    Work on some of the HID Led code.  Needs testing now.

--- protocol/hid/fan.c
+++ protocol/hid/fan.c
@@ -27,110 +27,110 @@
 extern CorsairLink *cl;
 
 int CorsairFan::SelectFan(int index) {
-	memset(cl->buf, 0x00, sizeof(cl->buf));
-	cl->buf[0] = 0x04;
-	cl->buf[1] = cl->CommandId++;
-	cl->buf[2] = WriteOneByte;
-	cl->buf[3] = FAN_Select;
-	cl->buf[4] = index;
+	memset(commands, 0x00, sizeof(commands));
+	commands[0] = 0x04;
+	commands[1] = cl->CommandId++;
+	commands[2] = WriteOneByte;
+	commands[3] = FAN_Select;
+	commands[4] = index;
 
-	cl->hid_wrapper(cl->handle, cl->buf, 8);
-	return cl->buf[2];
+	cl->hid_wrapper(cl->handle, commands, 8);
+	return commands[2];
 }
 
 int CorsairFan::CurrentFan() {
-	memset(cl->buf, 0x00, sizeof(cl->buf));
-	cl->buf[0] = 0x03;
-	cl->buf[1] = cl->CommandId++;
-	cl->buf[2] = ReadOneByte;
-	cl->buf[3] = FAN_Select;
+	memset(commands, 0x00, sizeof(commands));
+	commands[0] = 0x03;
+	commands[1] = cl->CommandId++;
+	commands[2] = ReadOneByte;
+	commands[3] = FAN_Select;
 
-	cl->hid_wrapper(cl->handle, cl->buf, 8);
-	return cl->buf[2];
+	cl->hid_wrapper(cl->handle, commands, 8);
+	return commands[2];
 }
 
 int CorsairFan::GetFanCount() {
-	memset(cl->buf, 0x00, sizeof(cl->buf));
-	cl->buf[0] = 0x03;
-	cl->buf[1] = cl->CommandId++;
-	cl->buf[2] = WriteOneByte;
-	cl->buf[3] = FAN_Count;
-	cl->buf[4] = 0x00;
+	memset(commands, 0x00, sizeof(commands));
+	commands[0] = 0x03;
+	commands[1] = cl->CommandId++;
+	commands[2] = WriteOneByte;
+	commands[3] = FAN_Count;
+	commands[4] = 0x00;
 
-	cl->hid_wrapper(cl->handle, cl->buf, 8);
-	return cl->buf[2];
+	cl->hid_wrapper(cl->handle, commands, 8);
+	return commands[2];
 }
 
 int CorsairFan::GetFanMode() {
-	memset(cl->buf, 0x00, sizeof(cl->buf));
-	cl->buf[0] = 0x03;
-	cl->buf[1] = cl->CommandId++;
-	cl->buf[2] = ReadOneByte;
-	cl->buf[3] = FAN_Mode;
-	cl->buf[4] = 0x00;
+	memset(commands, 0x00, sizeof(commands));
+	commands[0] = 0x03;
+	commands[1] = cl->CommandId++;
+	commands[2] = ReadOneByte;
+	commands[3] = FAN_Mode;
+	commands[4] = 0x00;
 
-	cl->hid_wrapper(cl->handle, cl->buf, 8);
-	return cl->buf[2];
+	cl->hid_wrapper(cl->handle, commands, 8);
+	return commands[2];
 }
 
 int CorsairFan::SetFanMode(int mode) {
-	memset(cl->buf, 0x00, sizeof(cl->buf));
-	cl->buf[0] = 0x03;
-	cl->buf[1] = cl->CommandId++;
-	cl->buf[2] = WriteOneByte;
-	cl->buf[3] = FAN_Mode;
-	cl->buf[4] = mode;
-	cl->hid_wrapper(cl->handle, cl->buf, 8);
+	memset(commands, 0x00, sizeof(commands));
+	commands[0] = 0x03;
+	commands[1] = cl->CommandId++;
+	commands[2] = WriteOneByte;
+	commands[3] = FAN_Mode;
+	commands[4] = mode;
+	cl->hid_wrapper(cl->handle, commands, 8);
 	return 0;
 }
 
 int CorsairFan::GetFanPWM() {
 	//ReadOneByte
-	memset(cl->buf, 0x00, sizeof(cl->buf));
-	cl->buf[0] = 0x03;
-	cl->buf[1] = cl->CommandId++;
-	cl->buf[2] = ReadOneByte;
-	cl->buf[3] = FAN_FixedPWM;
-	cl->hid_wrapper(cl->handle, cl->buf, 8);
-	return cl->buf[2];
+	memset(commands, 0x00, sizeof(commands));
+	commands[0] = 0x03;
+	commands[1] = cl->CommandId++;
+	commands[2] = ReadOneByte;
+	commands[3] = FAN_FixedPWM;
+	cl->hid_wrapper(cl->handle, commands, 8);
+	return commands[2];
 }
 
 int CorsairFan::SetFanPWM(int pwm) {
 	//WriteOneByte
-	memset(cl->buf, 0x00, sizeof(cl->buf));
-	cl->buf[0] = 0x04;
-	cl->buf[1] = cl->CommandId++;
-	cl->buf[2] = WriteOneByte;
-	cl->buf[3] = FAN_FixedPWM;
-	cl->buf[4] = pwm;
-	cl->hid_wrapper(cl->handle, cl->buf, 8);
+	memset(commands, 0x00, sizeof(commands));
+	commands[0] = 0x04;
+	commands[1] = cl->CommandId++;
+	commands[2] = WriteOneByte;
+	commands[3] = FAN_FixedPWM;
+	commands[4] = pwm;
+	cl->hid_wrapper(cl->handle, commands, 8);
 	return 0;
 }
 
 int CorsairFan::GetFanRPM() {
 	//ReadTwoBytes
-	memset(cl->buf, 0x00, sizeof(cl->buf));
-	cl->buf[0] = 0x04;
-	cl->buf[1] = cl->CommandId++;
-	cl->buf[2] = ReadTwoBytes;
-	cl->buf[3] = FAN_FixedRPM;
-	cl->buf[4] = 0x00;
-	cl->hid_wrapper(cl->handle, cl->buf, 8);
-	int RPM = cl->buf[3]<<8;
-	RPM += cl->buf[2];
+	memset(commands, 0x00, sizeof(commands));
+	commands[0] = 0x04;
+	commands[1] = cl->CommandId++;
+	commands[2] = ReadTwoBytes;
+	commands[3] = FAN_FixedRPM;
+	commands[4] = 0x00;
+	cl->hid_wrapper(cl->handle, commands, 8);
+	int RPM = commands[3]<<8;
+	RPM += commands[2];
 	return RPM;
 }
 
 int CorsairFan::SetFanRPM(int rpm) {
 	//WriteTwoBytes
-	memset(cl->buf, 0x00, sizeof(cl->buf));
-	cl->buf[0] = 0x05;
-	cl->buf[1] = cl->CommandId++;
-	cl->buf[2] = WriteTwoBytes;
-	cl->buf[3] = FAN_FixedRPM;
-	cl->buf[4] = rpm & 0x00ff;
-	cl->buf[5] = rpm>>8;
-	cl->hid_wrapper(cl->handle, cl->buf, 8);
+	memset(commands, 0x00, sizeof(commands));
+	commands[0] = 0x05;
+	commands[1] = cl->CommandId++;
+	commands[2] = WriteTwoBytes;
+	commands[3] = FAN_FixedRPM;
+	commands[4] = rpm & 0x00ff;
+	commands[5] = rpm>>8;
+	cl->hid_wrapper(cl->handle, commands, 8);
 	return 0;
 }
 
@@ -146,161 +146,161 @@ int CorsairFan::SetExternalReport() {
 
 int CorsairFan::GetCurrentRPM() {
 	//ReadTwoBytes
-	memset(cl->buf, 0x00, sizeof(cl->buf));	
-	cl->buf[0] = 0x03;
-	cl->buf[1] = cl->CommandId++;
-	cl->buf[2] = ReadTwoBytes;
-	cl->buf[3] = FAN_ReadRPM;
-	cl->hid_wrapper(cl->handle, cl->buf, 8);
-	int RPM = cl->buf[3]<<8;
-	RPM += cl->buf[2];
+	memset(commands, 0x00, sizeof(commands));	
+	commands[0] = 0x03;
+	commands[1] = cl->CommandId++;
+	commands[2] = ReadTwoBytes;
+	commands[3] = FAN_ReadRPM;
+	cl->hid_wrapper(cl->handle, commands, 8);
+	int RPM = commands[3]<<8;
+	RPM += commands[2];
 	return RPM;
 }
 
 int CorsairFan::GetMaxRPM() {
 	//ReadTwoBytes
-	memset(cl->buf, 0x00, sizeof(cl->buf));
-	cl->buf[0] = 0x03;
-	cl->buf[1] = cl->CommandId++;
-	cl->buf[2] = ReadTwoBytes;
-	cl->buf[3] = FAN_MaxRecordedRPM;
-	cl->hid_wrapper(cl->handle, cl->buf, 8);
-	int MaxRPM = cl->buf[3]<<8;
-	MaxRPM += cl->buf[2];
+	memset(commands, 0x00, sizeof(commands));
+	commands[0] = 0x03;
+	commands[1] = cl->CommandId++;
+	commands[2] = ReadTwoBytes;
+	commands[3] = FAN_MaxRecordedRPM;
+	cl->hid_wrapper(cl->handle, commands, 8);
+	int MaxRPM = commands[3]<<8;
+	MaxRPM += commands[2];
 	return MaxRPM;
 }
 
 int CorsairFan::GetFanUnderspeedThreshold() {
 	//ReadTwoBytes
-	memset(cl->buf, 0x00, sizeof(cl->buf));
-	cl->buf[0] = 0x03;
-	cl->buf[1] = cl->CommandId++;
-	cl->buf[2] = ReadTwoBytes;
-	cl->buf[3] = FAN_UnderSpeedThreshold;
-	cl->hid_wrapper(cl->handle, cl->buf, 8);
-	int threshold = cl->buf[3]<<8;
-	threshold += cl->buf[2];
+	memset(commands, 0x00, sizeof(commands));
+	commands[0] = 0x03;
+	commands[1] = cl->CommandId++;
+	commands[2] = ReadTwoBytes;
+	commands[3] = FAN_UnderSpeedThreshold;
+	cl->hid_wrapper(cl->handle, commands, 8);
+	int threshold = commands[3]<<8;
+	threshold += commands[2];
 	return 0;
 }
 
 int CorsairFan::SetFanUnderspeedThreshold(int threshold) {
 	//WriteTwoBytes
-	memset(cl->buf, 0x00, sizeof(cl->buf));
-	cl->buf[0] = 0x05;
-	cl->buf[1] = cl->CommandId++;
-	cl->buf[2] = WriteTwoBytes;
-	cl->buf[3] = FAN_UnderSpeedThreshold;
-	cl->buf[4] = threshold & 0x00FF;
-	cl->buf[5] = threshold >> 8;
+	memset(commands, 0x00, sizeof(commands));
+	commands[0] = 0x05;
+	commands[1] = cl->CommandId++;
+	commands[2] = WriteTwoBytes;
+	commands[3] = FAN_UnderSpeedThreshold;
+	commands[4] = threshold & 0x00FF;
+	commands[5] = threshold >> 8;
 	return 0;
 }
 
 int CorsairFan::GetRPMTable(int &rpm1, int &rpm2, int &rpm3, int &rpm4, int &rpm5) {
-	memset(cl->buf, 0x00, sizeof(cl->buf));
-	cl->buf[0] = 0x0F;
-	cl->buf[1] = cl->CommandId++;
-	cl->buf[2] = ReadThreeBytes;
-	cl->buf[3] = FAN_RPMTable;
-	cl->buf[4] = 0x0A;
-	cl->hid_wrapper(cl->handle, cl->buf, 24);
+	memset(commands, 0x00, sizeof(commands));
+	commands[0] = 0x0F;
+	commands[1] = cl->CommandId++;
+	commands[2] = ReadThreeBytes;
+	commands[3] = FAN_RPMTable;
+	commands[4] = 0x0A;
+	cl->hid_wrapper(cl->handle, commands, 24);
 
-	rpm1 = cl->buf[4]<<8;
-	rpm1 += cl->buf[3];
+	rpm1 = commands[4]<<8;
+	rpm1 += commands[3];
 
-	rpm2 = cl->buf[6]<<8;
-	rpm2 += cl->buf[5];
+	rpm2 = commands[6]<<8;
+	rpm2 += commands[5];
 
-	rpm3 = cl->buf[8]<<8;
-	rpm3 += cl->buf[7];
+	rpm3 = commands[8]<<8;
+	rpm3 += commands[7];
 
-	rpm4 = cl->buf[10]<<8;
-	rpm4 += cl->buf[9];
+	rpm4 = commands[10]<<8;
+	rpm4 += commands[9];
 
-	rpm5 = cl->buf[12]<<8;
-	rpm5 += cl->buf[11];
+	rpm5 = commands[12]<<8;
+	rpm5 += commands[11];
 
 	return 0;
 }
 
 int CorsairFan::SetRPMTable(int rpm1, int rpm2, int rpm3, int rpm4, int rpm5) {
-	memset(cl->buf, 0x00, sizeof(cl->buf));	
-	cl->buf[0] = 0x0F;
-	cl->buf[1] = cl->CommandId++;
-	cl->buf[2] = WriteThreeBytes;
-	cl->buf[3] = FAN_RPMTable;
-	cl->buf[4] = 0x0A;
+	memset(commands, 0x00, sizeof(commands));	
+	commands[0] = 0x0F;
+	commands[1] = cl->CommandId++;
+	commands[2] = WriteThreeBytes;
+	commands[3] = FAN_RPMTable;
+	commands[4] = 0x0A;
 
-	cl->buf[5] = rpm1 & 0x00FF;
-	cl->buf[6] = rpm1>>8;
+	commands[5] = rpm1 & 0x00FF;
+	commands[6] = rpm1>>8;
 
-	cl->buf[7] = rpm2 & 0x00FF;
-	cl->buf[8] = rpm2>>8;
+	commands[7] = rpm2 & 0x00FF;
+	commands[8] = rpm2>>8;
 
-	cl->buf[9] = rpm3 & 0x00FF;
-	cl->buf[10] = rpm3>>8;
+	commands[9] = rpm3 & 0x00FF;
+	commands[10] = rpm3>>8;
 
-	cl->buf[11] = rpm4 & 0x00FF;
-	cl->buf[12] = rpm4>>8;
+	commands[11] = rpm4 & 0x00FF;
+	commands[12] = rpm4>>8;
 
-	cl->buf[13] = rpm5 & 0x00FF;
-	cl->buf[14] = rpm5>>8;
+	commands[13] = rpm5 & 0x00FF;
+	commands[14] = rpm5>>8;
 
-	cl->hid_wrapper(cl->handle, cl->buf, 24);
+	cl->hid_wrapper(cl->handle, commands, 24);
 
 	return 0;
 }
 
 int CorsairFan::GetTemperatureTable(int &temp1, int &temp2, int &temp3, int &temp4, int &temp5) {
-	memset(cl->buf, 0x00, sizeof(cl->buf));
-	cl->buf[0] = 0x0F;
-	cl->buf[1] = cl->CommandId++;
-	cl->buf[2] = ReadThreeBytes;
-	cl->buf[3] = FAN_TempTable;
-	cl->buf[4] = 0x0A;
-	cl->hid_wrapper(cl->handle, cl->buf, 24);
+	memset(commands, 0x00, sizeof(commands));
+	commands[0] = 0x0F;
+	commands[1] = cl->CommandId++;
+	commands[2] = ReadThreeBytes;
+	commands[3] = FAN_TempTable;
+	commands[4] = 0x0A;
+	cl->hid_wrapper(cl->handle, commands, 24);
 
-	temp1 = cl->buf[4]<<8;
-	temp1 += cl->buf[3];
+	temp1 = commands[4]<<8;
+	temp1 += commands[3];
 
-	temp2 = cl->buf[6]<<8;
-	temp2 += cl->buf[5];
+	temp2 = commands[6]<<8;
+	temp2 += commands[5];
 
-	temp3 = cl->buf[8]<<8;
-	temp3 += cl->buf[7];
+	temp3 = commands[8]<<8;
+	temp3 += commands[7];
 
-	temp4 = cl->buf[10]<<8;
-	temp4 += cl->buf[9];
+	temp4 = commands[10]<<8;
+	temp4 += commands[9];
 
-	temp5 = cl->buf[12]<<8;
-	temp5 += cl->buf[11];
+	temp5 = commands[12]<<8;
+	temp5 += commands[11];
 
 	return 0;
 }
 
 int CorsairFan::SetTemperatureTable(int temp1, int temp2, int temp3, int temp4, int temp5) {
-	memset(cl->buf, 0x00, sizeof(cl->buf));	
-	cl->buf[0] = 0x0F;
-	cl->buf[1] = cl->CommandId++;
-	cl->buf[2] = WriteThreeBytes;
-	cl->buf[3] = FAN_TempTable;
-	cl->buf[4] = 0x0A;
+	memset(commands, 0x00, sizeof(commands));	
+	commands[0] = 0x0F;
+	commands[1] = cl->CommandId++;
+	commands[2] = WriteThreeBytes;
+	commands[3] = FAN_TempTable;
+	commands[4] = 0x0A;
 
-	cl->buf[5] = temp1 & 0x00FF;
-	cl->buf[6] = temp1>>8;
+	commands[5] = temp1 & 0x00FF;
+	commands[6] = temp1>>8;
 
-	cl->buf[7] = temp2 & 0x00FF;
-	cl->buf[8] = temp2>>8;
+	commands[7] = temp2 & 0x00FF;
+	commands[8] = temp2>>8;
 
-	cl->buf[9] = temp3 & 0x00FF;
-	cl->buf[10] = temp3>>8;
+	commands[9] = temp3 & 0x00FF;
+	commands[10] = temp3>>8;
 
-	cl->buf[11] = temp4 & 0x00FF;
-	cl->buf[12] = temp4>>8;
+	commands[11] = temp4 & 0x00FF;
+	commands[12] = temp4>>8;
 
-	cl->buf[13] = temp5 & 0x00FF;
-	cl->buf[14] = temp5>>8;
+	commands[13] = temp5 & 0x00FF;
+	commands[14] = temp5>>8;
 
-	cl->hid_wrapper(cl->handle, cl->buf, 24);
+	cl->hid_wrapper(cl->handle, commands, 24);
 
 	return 0;
 }
@@ -347,27 +347,27 @@ int CorsairFan::ConnectedFans() {
 	int fans = 0, i = 0, fanMode = 0;
 
 	for (i = 0; i < 5; i++) {
-		memset(cl->buf,0x00,sizeof(cl->buf));
+		memset(commands,0x00,sizeof(commands));
 		// Read fan Mode
-		cl->buf[0] = 0x07; // Length
-		cl->buf[1] = cl->CommandId++; // Command ID
-		cl->buf[2] = WriteOneByte; // Command Opcode
-		cl->buf[3] = FAN_Select; // Command data...
-		cl->buf[4] = i; // select fan
-		cl->buf[5] = cl->CommandId++; // Command ID
-		cl->buf[6] = ReadOneByte; // Command Opcode
-		cl->buf[7] = FAN_Mode; // Command data...
+		commands[0] = 0x07; // Length
+		commands[1] = cl->CommandId++; // Command ID
+		commands[2] = WriteOneByte; // Command Opcode
+		commands[3] = FAN_Select; // Command data...
+		commands[4] = i; // select fan
+		commands[5] = cl->CommandId++; // Command ID
+		commands[6] = ReadOneByte; // Command Opcode
+		commands[7] = FAN_Mode; // Command data...
 
-		int res = hid_write(cl->handle, cl->buf, 11);
+		int res = hid_write(cl->handle, commands, 11);
 		if (res < 0) {
 			fprintf(stderr, "Error: Unable to write() %s\n", (char*)hid_error(cl->handle) );
 		}
 
-		res = cl->hid_read_wrapper(cl->handle, cl->buf);
+		res = cl->hid_read_wrapper(cl->handle, commands);
 		if (res < 0) {
 			fprintf(stderr, "Error: Unable to read() %s\n", (char*)hid_error(cl->handle) );
 		}
-		fanMode = cl->buf[4];
+		fanMode = commands[4];
 
 		if(fanMode != 0x03){
 			fans++;
@@ -387,85 +387,85 @@ void CorsairFan::ReadFanInfo(int fanIndex, CorsairFanInfo *fan){
 		asprintf(&fan->Name, "Pump");
 	}
 
-	memset(cl->buf,0x00,sizeof(cl->buf));
+	memset(commands,0x00,sizeof(commands));
 	// Read fan Mode
-	cl->buf[0] = 0x07; // Length
-	cl->buf[1] = cl->CommandId++; // Command ID
-	cl->buf[2] = WriteOneByte; // Command Opcode
-	cl->buf[3] = FAN_Select; // Command data...
-	cl->buf[4] = fanIndex; // select fan
-	cl->buf[5] = cl->CommandId++; // Command ID
-	cl->buf[6] = ReadOneByte; // Command Opcode
-	cl->buf[7] = FAN_Mode; // Command data...
+	commands[0] = 0x07; // Length
+	commands[1] = cl->CommandId++; // Command ID
+	commands[2] = WriteOneByte; // Command Opcode
+	commands[3] = FAN_Select; // Command data...
+	commands[4] = fanIndex; // select fan
+	commands[5] = cl->CommandId++; // Command ID
+	commands[6] = ReadOneByte; // Command Opcode
+	commands[7] = FAN_Mode; // Command data...
 	
-	res = hid_write(cl->handle, cl->buf, 11);
+	res = hid_write(cl->handle, commands, 11);
 	if (res < 0) {
 		fprintf(stderr, "Error: Unable to write() %s\n", (char*)hid_error(cl->handle) );
 	}
 
-	res = cl->hid_read_wrapper(cl->handle, cl->buf);
+	res = cl->hid_read_wrapper(cl->handle, commands);
 	if (res < 0) {
 		fprintf(stderr, "Error: Unable to read() %s\n", (char*)hid_error(cl->handle) );
 	}
-	fan->Mode = cl->buf[4] & 0x0E;
+	fan->Mode = commands[4] & 0x0E;
 
-	memset(cl->buf,0x00,sizeof(cl->buf));
+	memset(commands,0x00,sizeof(commands));
 	// Read fan RPM
-	cl->buf[0] = 0x07; // Length
-	cl->buf[1] = cl->CommandId++; // Command ID
-	cl->buf[2] = WriteOneByte; // Command Opcode
-	cl->buf[3] = FAN_Select; // Command data...
-	cl->buf[4] = fanIndex; // select fan
-	cl->buf[5] = cl->CommandId++; // Command ID
-	cl->buf[6] = ReadTwoBytes; // Command Opcode
-	cl->buf[7] = FAN_ReadRPM; // Command data...
+	commands[0] = 0x07; // Length
+	commands[1] = cl->CommandId++; // Command ID
+	commands[2] = WriteOneByte; // Command Opcode
+	commands[3] = FAN_Select; // Command data...
+	commands[4] = fanIndex; // select fan
+	commands[5] = cl->CommandId++; // Command ID
+	commands[6] = ReadTwoBytes; // Command Opcode
+	commands[7] = FAN_ReadRPM; // Command data...
 
-	res = hid_write(cl->handle, cl->buf, 11);
+	res = hid_write(cl->handle, commands, 11);
 	if (res < 0) {
 		fprintf(stderr, "Error: Unable to write() %s\n", (char*)hid_error(cl->handle) );
 	}
 
-	res = cl->hid_read_wrapper(cl->handle, cl->buf);
+	res = cl->hid_read_wrapper(cl->handle, commands);
 	//All data is little-endian.
-	int rpm = cl->buf[5] << 8;
-	rpm += cl->buf[4];
+	int rpm = commands[5] << 8;
+	rpm += commands[4];
 
 	fan->RPM = rpm;
 }
 
 int CorsairFan::SetFansInfo(int fanIndex, CorsairFanInfo fanInfo){
-	memset(cl->buf,0x00,sizeof(cl->buf));
+	memset(commands,0x00,sizeof(commands));
 
 	if(fanInfo.Mode == FixedPWM || fanInfo.Mode == FixedRPM
 		|| fanInfo.Mode == Default || fanInfo.Mode == Quiet
 		|| fanInfo.Mode == Balanced	|| fanInfo.Mode == Performance
 		|| fanInfo.Mode == Custom) {
 
-		cl->buf[0] = 0x0b; // Length
-		cl->buf[1] = cl->CommandId++; // Command ID
-		cl->buf[2] = WriteOneByte; // Command Opcode
-		cl->buf[3] = FAN_Select; // Command data...
-		cl->buf[4] = fanIndex; // select fan
-		cl->buf[5] = cl->CommandId++; // Command ID
-		cl->buf[6] = WriteOneByte; // Command Opcode
-		cl->buf[7] = FAN_Mode; // Command data...
-		cl->buf[8] = fanInfo.Mode;
-		cl->buf[9] = cl->CommandId++; // Command ID
-		cl->buf[10] = ReadOneByte; // Command Opcode
-		cl->buf[11] = FAN_Mode; // Command data...
+		commands[0] = 0x0b; // Length
+		commands[1] = cl->CommandId++; // Command ID
+		commands[2] = WriteOneByte; // Command Opcode
+		commands[3] = FAN_Select; // Command data...
+		commands[4] = fanIndex; // select fan
+		commands[5] = cl->CommandId++; // Command ID
+		commands[6] = WriteOneByte; // Command Opcode
+		commands[7] = FAN_Mode; // Command data...
+		commands[8] = fanInfo.Mode;
+		commands[9] = cl->CommandId++; // Command ID
+		commands[10] = ReadOneByte; // Command Opcode
+		commands[11] = FAN_Mode; // Command data...
 
-		int res = hid_write(cl->handle, cl->buf, 17);
+		int res = hid_write(cl->handle, commands, 17);
 		if (res < 0) {
 			fprintf(stderr, "%s", (char*)hid_error(cl->handle) );
 			return 1;
 		}
 
-		res = cl->hid_read_wrapper(cl->handle, cl->buf);
+		res = cl->hid_read_wrapper(cl->handle, commands);
 		if (res < 0) {
 			fprintf(stderr, "%s", (char*)hid_error(cl->handle) );
 			return 1;
 		}
-		if(fanInfo.Mode != cl->buf[6]){
+		if(fanInfo.Mode != commands[6]){
 			fprintf(stderr, "Cannot set fan mode.");
 			return 1;
 		}
@@ -474,36 +474,36 @@ int CorsairFan::SetFansInfo(int fanIndex, CorsairFanInfo fanInfo){
 		return 1;
 	}
 	if(fanInfo.RPM != 0) {
-		memset(cl->buf,0x00,sizeof(cl->buf));
+		memset(commands,0x00,sizeof(commands));
 
-		cl->buf[0] = 0x0b; // Length
-		cl->buf[1] = cl->CommandId++; // Command ID
-		cl->buf[2] = WriteOneByte; // Command Opcode
-		cl->buf[3] = FAN_Select; // Command data...
-		cl->buf[4] = fanIndex; // select fan
-		cl->buf[5] = cl->CommandId++; // Command ID
-		cl->buf[6] = WriteTwoBytes; // Command Opcode
-		cl->buf[7] = FAN_FixedRPM; // Command data...
-		cl->buf[8] = fanInfo.RPM & 0x00FF;
-		cl->buf[9] = fanInfo.RPM >> 8;
-		cl->buf[10] = cl->CommandId++; // Command ID
-		cl->buf[11] = ReadTwoBytes; // Command Opcode
-		cl->buf[12] = FAN_ReadRPM; // Command data...
+		commands[0] = 0x0b; // Length
+		commands[1] = cl->CommandId++; // Command ID
+		commands[2] = WriteOneByte; // Command Opcode
+		commands[3] = FAN_Select; // Command data...
+		commands[4] = fanIndex; // select fan
+		commands[5] = cl->CommandId++; // Command ID
+		commands[6] = WriteTwoBytes; // Command Opcode
+		commands[7] = FAN_FixedRPM; // Command data...
+		commands[8] = fanInfo.RPM & 0x00FF;
+		commands[9] = fanInfo.RPM >> 8;
+		commands[10] = cl->CommandId++; // Command ID
+		commands[11] = ReadTwoBytes; // Command Opcode
+		commands[12] = FAN_ReadRPM; // Command data...
 
-		int res = hid_write(cl->handle, cl->buf, 18);
+		int res = hid_write(cl->handle, commands, 18);
 		if (res < 0) {
 			fprintf(stderr, "%s", (char*)hid_error(cl->handle) );
 			return 1;
 		}
 
-		res = cl->hid_read_wrapper(cl->handle, cl->buf);
+		res = cl->hid_read_wrapper(cl->handle, commands);
 		if (res < 0) {
 			fprintf(stderr, "%s", (char*)hid_error(cl->handle) );
 			return 1;
 		}
 		//All data is little-endian.
-		int rpm = cl->buf[5] << 8;
-		rpm += cl->buf[4];
+		int rpm = commands[5] << 8;
+		rpm += commands[4];
 		if(fanInfo.RPM != rpm){
 			fprintf(stderr, "Cannot set fan RPM.");
 			return 1;
--- protocol/hid/temp.c
+++ protocol/hid/temp.c
@@ -28,147 +28,147 @@ extern CorsairLink *cl;
 
 int CorsairTemp::SelectSensor(int index)
 {
-	memset(cl->buf,0x00,sizeof(cl->buf));
+	memset(commands,0x00,sizeof(commands));
 
-	cl->buf[0] = 0x04; // Length
-	cl->buf[1] = cl->CommandId++; // Command ID
-	cl->buf[2] = WriteOneByte; // Command Opcode
-	cl->buf[3] = TEMP_SelectActiveSensor; // Command data...
-	cl->buf[4] = index;
+	commands[0] = 0x04; // Length
+	commands[1] = cl->CommandId++; // Command ID
+	commands[2] = WriteOneByte; // Command Opcode
+	commands[3] = TEMP_SelectActiveSensor; // Command data...
+	commands[4] = index;
 
-	int res = hid_write(cl->handle, cl->buf, 11);
+	int res = hid_write(cl->handle, commands, 11);
 	if (res < 0) {
 		fprintf(stderr, "Error: Unable to write() %s\n", (char*)hid_error(cl->handle) );
 		//return -1;
 	}
 
-	res = cl->hid_read_wrapper(cl->handle, cl->buf);
+	res = cl->hid_read_wrapper(cl->handle, commands);
 	if (res < 0) {
 		fprintf(stderr, "Error: Unable to read() %s\n", (char*)hid_error(cl->handle) );
 		//return -1;
 	}
 
-	return cl->buf[2];
+	return commands[2];
 }
 
 int CorsairTemp::CurrentSensor()
 {
-	memset(cl->buf,0x00,sizeof(cl->buf));
+	memset(commands,0x00,sizeof(commands));
 
-	cl->buf[0] = 0x03; // Length
-	cl->buf[1] = cl->CommandId++; // Command ID
-	cl->buf[2] = ReadOneByte; // Command Opcode
-	cl->buf[3] = TEMP_SelectActiveSensor; // Command data...
+	commands[0] = 0x03; // Length
+	commands[1] = cl->CommandId++; // Command ID
+	commands[2] = ReadOneByte; // Command Opcode
+	commands[3] = TEMP_SelectActiveSensor; // Command data...
 
-	int res = hid_write(cl->handle, cl->buf, 11);
+	int res = hid_write(cl->handle, commands, 11);
 	if (res < 0) {
 		fprintf(stderr, "Error: Unable to write() %s\n", (char*)hid_error(cl->handle) );
 		//return -1;
 	}
 
-	res = cl->hid_read_wrapper(cl->handle, cl->buf);
+	res = cl->hid_read_wrapper(cl->handle, commands);
 	if (res < 0) {
 		fprintf(stderr, "Error: Unable to read() %s\n", (char*)hid_error(cl->handle) );
 		//return -1;
 	}
 
-	return cl->buf[2];
+	return commands[2];
 }
 
 int CorsairTemp::GetTempSensors()
 {//1
-	memset(cl->buf,0x00,sizeof(cl->buf));
+	memset(commands,0x00,sizeof(commands));
 	// Read fan Mode
-	cl->buf[0] = 0x03; // Length
-	cl->buf[1] = cl->CommandId++; // Command ID
-	cl->buf[2] = ReadOneByte; // Command Opcode
-	cl->buf[3] = TEMP_CountSensors; // Command data...
+	commands[0] = 0x03; // Length
+	commands[1] = cl->CommandId++; // Command ID
+	commands[2] = ReadOneByte; // Command Opcode
+	commands[3] = TEMP_CountSensors; // Command data...
 
-	int res = hid_write(cl->handle, cl->buf, 11);
+	int res = hid_write(cl->handle, commands, 11);
 	if (res < 0) {
 		fprintf(stderr, "Error: Unable to write() %s\n", (char*)hid_error(cl->handle) );
 		//return -1;
 	}
 
-	res = cl->hid_read_wrapper(cl->handle, cl->buf);
+	res = cl->hid_read_wrapper(cl->handle, commands);
 	if (res < 0) {
 		fprintf(stderr, "Error: Unable to read() %s\n", (char*)hid_error(cl->handle) );
 		//return -1;
 	}
 
-	return cl->buf[2];
+	return commands[2];
 }
 
 int CorsairTemp::GetTemp()
 {//2
-	memset(cl->buf,0x00,sizeof(cl->buf));
+	memset(commands,0x00,sizeof(commands));
 	// Read fan Mode
-	cl->buf[0] = 0x03; // Length
-	cl->buf[1] = cl->CommandId++; // Command ID
-	cl->buf[2] = ReadTwoBytes; // Command Opcode
-	cl->buf[3] = TEMP_Read; // Command data...
+	commands[0] = 0x03; // Length
+	commands[1] = cl->CommandId++; // Command ID
+	commands[2] = ReadTwoBytes; // Command Opcode
+	commands[3] = TEMP_Read; // Command data...
 
-	int res = hid_write(cl->handle, cl->buf, 11);
+	int res = hid_write(cl->handle, commands, 11);
 	if (res < 0) {
 		fprintf(stderr, "Error: Unable to write() %s\n", (char*)hid_error(cl->handle) );
 		//return -1;
 	}
 
-	res = cl->hid_read_wrapper(cl->handle, cl->buf);
+	res = cl->hid_read_wrapper(cl->handle, commands);
 	if (res < 0) {
 		fprintf(stderr, "Error: Unable to read() %s\n", (char*)hid_error(cl->handle) );
 		//return -1;
 	}
-	int temp = cl->buf[3]<<8;
-	temp += cl->buf[2];
+	int temp = commands[3]<<8;
+	temp += commands[2];
 	return temp;
 }
 
 int CorsairTemp::GetTempLimit()
 {//2
-	memset(cl->buf,0x00,sizeof(cl->buf));
+	memset(commands,0x00,sizeof(commands));
 	// Read fan Mode
-	cl->buf[0] = 0x03; // Length
-	cl->buf[1] = cl->CommandId++; // Command ID
-	cl->buf[2] = ReadTwoBytes; // Command Opcode
-	cl->buf[3] = TEMP_Limit; // Command data...
+	commands[0] = 0x03; // Length
+	commands[1] = cl->CommandId++; // Command ID
+	commands[2] = ReadTwoBytes; // Command Opcode
+	commands[3] = TEMP_Limit; // Command data...
 
-	int res = hid_write(cl->handle, cl->buf, 11);
+	int res = hid_write(cl->handle, commands, 11);
 	if (res < 0) {
 		fprintf(stderr, "Error: Unable to write() %s\n", (char*)hid_error(cl->handle) );
 		//return -1;
 	}
 
-	res = cl->hid_read_wrapper(cl->handle, cl->buf);
+	res = cl->hid_read_wrapper(cl->handle, commands);
 	if (res < 0) {
 		fprintf(stderr, "Error: Unable to read() %s\n", (char*)hid_error(cl->handle) );
 		//return -1;
 	}
 
-	int limit = cl->buf[5]<<8;
-	limit += cl->buf[4];
+	int limit = commands[5]<<8;
+	limit += commands[4];
 
 	return limit;
 }
 
 int CorsairTemp::SetTempLimit(int limit)
 {//2
-	memset(cl->buf,0x00,sizeof(cl->buf));
+	memset(commands,0x00,sizeof(commands));
 	// Read fan Mode
-	cl->buf[0] = 0x05; // Length
-	cl->buf[1] = cl->CommandId++; // Command ID
-	cl->buf[2] = WriteTwoBytes; // Command Opcode
-	cl->buf[3] = TEMP_Limit; // Command data...
-	cl->buf[4] = limit & 0x00FF;
-	cl->buf[5] = limit >> 8;
+	commands[0] = 0x05; // Length
+	commands[1] = cl->CommandId++; // Command ID
+	commands[2] = WriteTwoBytes; // Command Opcode
+	commands[3] = TEMP_Limit; // Command data...
+	commands[4] = limit & 0x00FF;
+	commands[5] = limit >> 8;
 
-	int res = hid_write(cl->handle, cl->buf, 11);
+	int res = hid_write(cl->handle, commands, 11);
 	if (res < 0) {
 		fprintf(stderr, "Error: Unable to write() %s\n", (char*)hid_error(cl->handle) );
 		return -1;
 	}
 
-	res = cl->hid_read_wrapper(cl->handle, cl->buf);
+	res = cl->hid_read_wrapper(cl->handle, commands);
 	if (res < 0) {
 		fprintf(stderr, "Error: Unable to read() %s\n", (char*)hid_error(cl->handle) );
 		return -1;

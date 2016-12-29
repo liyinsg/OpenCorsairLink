7ad802937355d09fb8ed1fab8bf4e9c046a6e431
Sean Nelson <audiohacked@gmail.com>
2016-12-28 17:32:41 -0800

    comment out old HID core code.

--- protocol/hid/core.c
+++ protocol/hid/core.c
@@ -23,98 +23,98 @@
 
 #include "core.h"
 
-int GetDeviceId(void)
-{
-	memset(buf,0,sizeof(buf));
-
-	// Read Device ID: 0x3b = H80i. 0x3c = H100i. 0x41 = H110i
-	buf[0] = 0x03; // Length
-	buf[1] = this->CommandId++; // Command ID
-	buf[2] = ReadOneByte; // Command Opcode
-	buf[3] = DeviceID; // Command data...
-	buf[4] = 0x00;
-
-	int res = hid_write(handle, buf, 17);
-	if (res < 0) {
-		fprintf(stderr, "Error: Unable to write() %s\n", (char*)hid_error(handle) );
-	}
-
-	hid_read_wrapper(handle, buf);
-	if (res < 0) {
-		fprintf(stderr, "Error: Unable to read() %s\n", (char*)hid_error(handle) );
-	}
-
-	return buf[2];
-}
-
-int GetFirmwareVersion()
-{
-	memset(buf,0,sizeof(buf));
-
-	// Read Device ID: 0x3b = H80i. 0x3c = H100i. 0x41 = H110i
-	buf[0] = 0x03; // Length
-	buf[1] = this->CommandId++; // Command ID
-	buf[2] = ReadTwoBytes; // Command Opcode
-	buf[3] = FirmwareID; // Command data...
-	buf[4] = 0x00;
-
-	int res = hid_write(handle, buf, 17);
-	if (res < 0) {
-		fprintf(stderr, "Error: Unable to write() %s\n", (char*)hid_error(handle) );
-	}
-
-	hid_read_wrapper(handle, buf);
-	if (res < 0) {
-		fprintf(stderr, "Error: Unable to read() %s\n", (char*)hid_error(handle) );
-	}
-	int firmware = buf[3]<<8;
-	firmware += buf[2];
-	return firmware;
-}
-
-int GetProductName(char *ostring)
-{
-	memset(buf,0,sizeof(buf));
-
-	// Read Device ID: 0x3b = H80i. 0x3c = H100i. 0x41 = H110i
-	buf[0] = 0x04; // Length
-	buf[1] = this->CommandId++; // Command ID
-	buf[2] = ReadThreeBytes; // Command Opcode
-	buf[3] = ProductName; // Command data...
-	buf[4] = 0x08;
-
-	int res = hid_write(handle, buf, 8);
-	if (res < 0) {
-		fprintf(stderr, "Error: Unable to write() %s\n", (char*)hid_error(handle) );
-	}
-
-	hid_read_wrapper(handle, buf);
-	if (res < 0) {
-		fprintf(stderr, "Error: Unable to read() %s\n", (char*)hid_error(handle) );
-	}
-	memcpy(ostring, buf + 3, 8);
-	return 0;
-}
-
-int GetDeviceStatus()
-{
-	memset(buf,0,sizeof(buf));
-
-	buf[0] = 0x03; // Length
-	buf[1] = this->CommandId++; // Command ID
-	buf[2] = ReadOneByte; // Command Opcode
-	buf[3] = Status; // Command data...
-	buf[4] = 0x00;
-
-	int res = hid_write(handle, buf, 17);
-	if (res < 0) {
-		fprintf(stderr, "Error: Unable to write() %s\n", (char*)hid_error(handle) );
-	}
-
-	hid_read_wrapper(handle, buf);
-	if (res < 0) {
-		fprintf(stderr, "Error: Unable to read() %s\n", (char*)hid_error(handle) );
-	}
-
-	return buf[2];
-}
+// int GetDeviceId(void)
+// {
+// 	memset(buf,0,sizeof(buf));
+//
+// 	// Read Device ID: 0x3b = H80i. 0x3c = H100i. 0x41 = H110i
+// 	buf[0] = 0x03; // Length
+// 	buf[1] = this->CommandId++; // Command ID
+// 	buf[2] = ReadOneByte; // Command Opcode
+// 	buf[3] = DeviceID; // Command data...
+// 	buf[4] = 0x00;
+//
+// 	int res = hid_write(handle, buf, 17);
+// 	if (res < 0) {
+// 		fprintf(stderr, "Error: Unable to write() %s\n", (char*)hid_error(handle) );
+// 	}
+//
+// 	hid_read_wrapper(handle, buf);
+// 	if (res < 0) {
+// 		fprintf(stderr, "Error: Unable to read() %s\n", (char*)hid_error(handle) );
+// 	}
+//
+// 	return buf[2];
+// }
+//
+// int GetFirmwareVersion()
+// {
+// 	memset(buf,0,sizeof(buf));
+//
+// 	// Read Device ID: 0x3b = H80i. 0x3c = H100i. 0x41 = H110i
+// 	buf[0] = 0x03; // Length
+// 	buf[1] = this->CommandId++; // Command ID
+// 	buf[2] = ReadTwoBytes; // Command Opcode
+// 	buf[3] = FirmwareID; // Command data...
+// 	buf[4] = 0x00;
+//
+// 	int res = hid_write(handle, buf, 17);
+// 	if (res < 0) {
+// 		fprintf(stderr, "Error: Unable to write() %s\n", (char*)hid_error(handle) );
+// 	}
+//
+// 	hid_read_wrapper(handle, buf);
+// 	if (res < 0) {
+// 		fprintf(stderr, "Error: Unable to read() %s\n", (char*)hid_error(handle) );
+// 	}
+// 	int firmware = buf[3]<<8;
+// 	firmware += buf[2];
+// 	return firmware;
+// }
+//
+// int GetProductName(char *ostring)
+// {
+// 	memset(buf,0,sizeof(buf));
+//
+// 	// Read Device ID: 0x3b = H80i. 0x3c = H100i. 0x41 = H110i
+// 	buf[0] = 0x04; // Length
+// 	buf[1] = this->CommandId++; // Command ID
+// 	buf[2] = ReadThreeBytes; // Command Opcode
+// 	buf[3] = ProductName; // Command data...
+// 	buf[4] = 0x08;
+//
+// 	int res = hid_write(handle, buf, 8);
+// 	if (res < 0) {
+// 		fprintf(stderr, "Error: Unable to write() %s\n", (char*)hid_error(handle) );
+// 	}
+//
+// 	hid_read_wrapper(handle, buf);
+// 	if (res < 0) {
+// 		fprintf(stderr, "Error: Unable to read() %s\n", (char*)hid_error(handle) );
+// 	}
+// 	memcpy(ostring, buf + 3, 8);
+// 	return 0;
+// }
+//
+// int GetDeviceStatus()
+// {
+// 	memset(buf,0,sizeof(buf));
+//
+// 	buf[0] = 0x03; // Length
+// 	buf[1] = this->CommandId++; // Command ID
+// 	buf[2] = ReadOneByte; // Command Opcode
+// 	buf[3] = Status; // Command data...
+// 	buf[4] = 0x00;
+//
+// 	int res = hid_write(handle, buf, 17);
+// 	if (res < 0) {
+// 		fprintf(stderr, "Error: Unable to write() %s\n", (char*)hid_error(handle) );
+// 	}
+//
+// 	hid_read_wrapper(handle, buf);
+// 	if (res < 0) {
+// 		fprintf(stderr, "Error: Unable to read() %s\n", (char*)hid_error(handle) );
+// 	}
+//
+// 	return buf[2];
+// }
